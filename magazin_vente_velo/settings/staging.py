be809dbf0077adbd1ff27c516de2c209f61af06b
Your Name <you@example.com>
2025-04-20 17:42:08 +0200

    migration reussite en staging

---
+++ magazin_vente_velo/settings/staging.py
@@ -0,0 +1,18 @@
+from .base import *
+import dj_database_url
+from .base import config
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+
+    'default': dj_database_url.parse(
+        
+        config("DATABASE_URL"),
+        conn_max_age=600,  # optionnel, pour les connexions persistantes
+        engine='django.contrib.gis.db.backends.postgis'
+    
+    )
+}
+
